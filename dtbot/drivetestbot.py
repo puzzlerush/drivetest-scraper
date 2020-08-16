f5fcb5f9e97d362061a4a99dcc65781dc4a8aa9d
tacticaltofu <dingkevin@gmail.com>
2020-08-16 19:40:14 -0400

    make wait timeout an integer

--- dtbot/drivetestbot.py
+++ dtbot/drivetestbot.py
@@ -24,7 +24,7 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.binary_location = GOOGLE_CHROME_PATH
 
-WAIT_TIMEOUT = os.environ.get('WAIT_TIMEOUT')
+WAIT_TIMEOUT = int(os.environ.get('WAIT_TIMEOUT'))
 
 book_test_xpath = "//a[@title='Book a Road Test']"
 continue_btn_xpath = "//button[.//span[text()='Continue']]"
