9eeed3aae3a58111b4e21a7fdbfa6533618172bc
tacticaltofu <dingkevin@gmail.com>
2020-08-16 21:20:27 -0400

    browser starts maximized

--- dtbot/drivetestbot.py
+++ dtbot/drivetestbot.py
@@ -21,6 +21,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.binary_location = GOOGLE_CHROME_PATH
 
